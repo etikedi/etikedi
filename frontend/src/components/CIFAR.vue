4c963a78cf69c390af6a12d78c6ee7e6ce7ff98a
janik.schoenfelder <jschoenfelder@posteo.de>
2020-04-29 13:37:56 +0200

    Upload missing files

---
+++ frontend/src/components/CIFAR.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="section">
+        <div class="container">
+            <img :src="image" />
+        </div>
+    </section>
+</template>
+
+<script>
+import {mapState} from "vuex";
+export default {
+    name: "CIFAR",
+    computed: {
+        ...mapState(["image"])
+    },
+    methods: {
+        loadImage: function(event) {
+            console.log(event);
+        }
+    }
+};
+</script>
+
+<style scoped></style>
