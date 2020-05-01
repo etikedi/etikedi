f749f69b76c54806b3bc3de303c8713f7eaa4d27
janik.schoenfelder <jschoenfelder@posteo.de>
2020-05-01 11:52:06 +0200

    Implement waiting animation

--- frontend/src/components/CIFAR.vue
+++ frontend/src/components/CIFAR.vue
@@ -2,21 +2,19 @@
     <section class="section">
         <div class="container">
             <img :src="image" />
+            <b-loading :is-full-page="false" :active.sync="loading" :can-cancel="false">
+            </b-loading>
         </div>
     </section>
 </template>
 
 <script>
 import {mapState} from "vuex";
+
 export default {
     name: "CIFAR",
     computed: {
-        ...mapState(["image"])
-    },
-    methods: {
-        loadImage: function(event) {
-            console.log(event);
-        }
+        ...mapState(["loading", "image"])
     }
 };
 </script>
