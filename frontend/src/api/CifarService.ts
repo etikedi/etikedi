83841ea49290947adb957e08405efd5873b9f332
janik.schoenfelder <jschoenfelder@posteo.de>
2020-05-12 14:54:15 +0200

    Update node sass

--- frontend/src/api/CifarService.ts
+++ frontend/src/api/CifarService.ts
@@ -2,29 +2,45 @@ import Api from "@/api/api";
 
 export default {
     getAllDatasets(): Promise<any> {
-        return Api().get("/dataset");
+        // return Api().get("/datasets");
+        return new Promise((resolve) => {
+            resolve({
+                datasets: [
+                    {
+                        "id": 27138,
+                        "name": "DWTC"
+                    },
+                    {
+                        "id": 45632,
+                        "name": "CIFAR"
+                    }
+                ]
+            });
+        });
     },
 
     getSampleID(params: {datasetID: string}): Promise<any> {
-        return Api().get("/dataset/" + params.datasetID);
+        return Api().get("/" + params.datasetID);
     },
 
     getLabels(params: {datasetID: string}): Promise<any> {
-        return Api().get("/dataset/" + params.datasetID + "/labels");
+        return Api().get("/" + params.datasetID + "/labels");
     },
 
     getSample(params: {sampleID: any}): Promise<any> {
         return Api().get("/sample/" + params.sampleID);
     },
 
-    updateSample(params: {
+    labelSample(params: {
         sampleID: string;
         labelID: any;
         userID: any;
     }): Promise<any> {
         return Api().post("/sample/" + params.sampleID, {
-            labelID: params.labelID,
-            userID: params.userID
+            association: {
+                "label_id": params.labelID,
+                "user_id": params.userID
+            }
         });
     }
 };
