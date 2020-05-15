a53a47e7e03a69831bc22c8036e842f6f3ed3027
sympathikus <roflpauli@hotmail.de>
2020-05-15 13:43:33 +0200

    Added model for flower of iris dataset (for testing)

--- backend/models/label_queue.py
+++ backend/models/label_queue.py
@@ -1,6 +1,15 @@
-from flask_restful import Resource
-from ..aergia import db
+from db_init import db
 
 
 class LabelQueue(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+
+
+class Flower(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sepal_length = db.Column(db.Float)
+    sepal_width = db.Column(db.Float)
+    petal_length = db.Column(db.Float)
+    petal_width = db.Column(db.Float)
+    label = db.Column(db.Integer)
+
