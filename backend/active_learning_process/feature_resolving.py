d09775af1665c29e642d7ffe765ee650050505cc
sympathikus <roflpauli@hotmail.de>
2020-05-15 13:46:36 +0200

    Added class to resolve features of different datatypes

--- backend/active_learning_process/feature_resolving.py
+++ backend/active_learning_process/feature_resolving.py
@@ -1,12 +1,13 @@
 class FeatureResolver:
 
-    def __init__(self, dataset, features):
+    def __init__(self, dataset, features, sample_ids):
         self.dataset = dataset
         self.features = features
+        self.sample_ids = sample_ids
 
     def resolve(self):
         # TODO Features-Resolving
-        resolved_features = []
+        resolved_features = [self.features[sample_id] for sample_id in self.sample_ids]
         feature_names = []
         if self.dataset == "cifar":
             pass
