1677d513ddfde3ef59a6434566c014b0dcb59a51
Nick Lehmann <nick.lehmann@mailbox.tu-dresden.de>
2020-05-16 14:24:37 +0200

    Add dummy users

--- backend/aergia.py
+++ backend/aergia.py
@@ -11,6 +11,33 @@ with app.app_context():
     db.init_app(app)
     db.create_all()
 
+    # Create the dummy users if none already exists
+    if not User.query.count():
+        app.logger.info('Creating dummy users')
+        admin = User(
+            username='ernst_haft',
+            password=guard.hash_password('adminadmin'),
+            roles='admin'
+        )
+        db.session.add(admin)
+
+        worker1 = User(
+            username='anna_lühse',
+            password=guard.hash_password('very_secret'),
+            roles='worker'
+        )
+        db.session.add(worker1)
+
+        worker2 = User(
+            username='mario_nette',
+            password=guard.hash_password('very_secret'),
+            roles='worker'
+        )
+        db.session.add(worker2)
+
+        db.session.commit()
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
 
