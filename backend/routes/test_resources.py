3aa63137cab9d2e58d78baa1b781ba946f148ca6
Paul Sikorski <roflpauli@hotmail.de>
2020-05-29 14:31:53 +0200

    Updated test resources to accept process resource returned by ProcessManager

--- backend/routes/test_resources.py
+++ backend/routes/test_resources.py
@@ -3,21 +3,19 @@ import random
 from flask import request
 from flask_restful import Resource
 
-from ..config import api
-from ..active_learning_process.process_management import ProcessManager
-
-manager = ProcessManager()
+from backend.config import api
+from backend.active_learning_process.process_management import manager
 
 
 class Index(Resource):
 
     def get(self):
         sample_id = []
-        backend_endpoint = manager.get_or_else_load("iris")
+        process_resources = manager.get_or_else_load("iris")
         print("Backend:\tQuerying process for dataset iris")
-        if backend_endpoint.poll(5):
+        if process_resources["pipe"].poll(5):
             print("Backend:\tFound new datapoints")
-            sample_id = backend_endpoint.recv()
+            sample_id = process_resources["pipe"].recv()
         else:
             print("Backend:\tNo samples available atm")
         return sample_id
@@ -28,9 +26,9 @@ class New(Resource):
     def get(self):
         id = request.args.get("sample", default=0, type=int)
         label = random.randint(0, 2)
-        backend_endoint = manager.get_or_else_load("iris")
+        process_resources = manager.get_or_else_load("iris")
         data = {"id": id, "label": label, "user": 1}
-        backend_endoint.send(data)
+        process_resources["pipe"].send(data)
         return data
 
 
