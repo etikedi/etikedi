9d01c3162e63c6f0cf14ffc999218203af68b95c
Paul Sikorski <roflpauli@hotmail.de>
2020-06-14 13:54:51 +0200

    Updated imports

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,5 +1,5 @@
 from .association import Association, AssociationSchema
 from .dataset import Dataset, DatasetSchema
-from .datatypes import Table, Sample, SampleSchema
+from .datatypes import Table, Sample, SampleSchema, Text, Image
 from .label import Label, LabelSchema
 from .user import User
