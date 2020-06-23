7ba0eaed600b8ac1cf746b4430ceb1da48e4cdc8
Franziskus <borrmann.f@web.de>
2020-06-23 20:09:56 +0200

    improved CIFAR view
    - added forward/backward navigation through samples by clicking on previous/next sample
    - added labeling information to samples
    - set focus to input field
    - added suggestions for input field

--- frontend/src/components/CIFAR/CIFAR.vue
+++ frontend/src/components/CIFAR/CIFAR.vue
@@ -8,15 +8,19 @@
         <div class="container">
             <div class="columns is-desktop is-vcentered">
                 <!-- left preview -->
-
-                <div v-if="count>0" class="box" id="preview">
+                <div v-if="count>0" class="box" id="preview" v-on:click="previous()">
+                    Sample-ID: {{samples[count-1].id}}
                     <div class="card-content">
                         <img
-                            class="image is-128x128 is-horizontal-center"
+                            class="image is-64x64 is-horizontal-center"
                             :src="samples[count-1].src"
                         />
                     </div>
-                    Label-ID: {{samples[count-1].labelId}}
+                    <div v-if="samples[count-1].labelId != null">
+                        Label-ID: {{samples[count-1].labelId}}
+                        <br />
+                        Label-Name: {{getLabelByID(samples[count-1].labelId).name}}
+                    </div>
                 </div>
 
                 <div class="box">
@@ -26,31 +30,41 @@
                             :src="samples[count].src"
                         />
                     </div>
-
-                    <div class="field">
-                        <input
+                    Sample-ID: {{samples[count].id}}
+                    <br />
+                    <br />
+                    <b-field label="Label:">
+                        <b-autocomplete
+                            id="input-field"
+                            class="is-fullwidth"
                             v-model="labelName"
-                            class="input is-fullwidth"
-                            type="text"
+                            :data="filteredDataArray"
                             placeholder="Label"
-                        />
-
-                    </div>
-
+                            clearable
+                            @select="option => selected = option"
+                        >
+                            <template slot="empty">No results found</template>
+                        </b-autocomplete>
+                    </b-field>
                     <div class="field">
                         <button v-on:click="click()" class="button is-info is-fullwidth">Send</button>
                     </div>
                 </div>
 
                 <!-- right preview -->
-                <div v-if="count+1<=maxCount" class="box" id="preview">
+                <div v-if="count+1<=maxCount" class="box" id="preview" v-on:click="next()">
+                    Sample-ID: {{samples[count+1].id}}
                     <div class="card-content">
                         <img
-                            class="image is-128x128 is-horizontal-center"
+                            class="image is-64x64 is-horizontal-center"
                             :src="samples[count+1].src"
                         />
                     </div>
-                    Label-ID: {{samples[count+1].labelId}}
+                    <div v-if="samples[count+1].labelId != null">
+                        Label-ID: {{samples[count+1].labelId}}
+                        <br />
+                        Label-Name: {{getLabelByID(samples[count+1].labelId).name}}
+                    </div>
                 </div>
             </div>
 
@@ -58,7 +72,7 @@
                         :is-full-page="false"
                         :active.sync="loading"
                         :can-cancel="false"
-                    ></b-loading> -->
+            ></b-loading>-->
         </div>
     </section>
 </template>
@@ -74,55 +88,65 @@ export default Vue.extend({
         const samples = [
             {
                 id: 0,
+                labelId: 0,
                 src:
                     "https://www.cs.toronto.edu/~kriz/cifar-10-sample/airplane4.png"
             },
             {
                 id: 1,
+                labelId: null,
                 src: "https://www.cs.toronto.edu/~kriz/cifar-10-sample/dog2.png"
             },
             {
                 id: 2,
+                labelId: null,
                 src:
                     "https://www.cs.toronto.edu/~kriz/cifar-10-sample/automobile4.png"
             },
             {
                 id: 3,
+                labelId: null,
                 src:
                     "https://www.cs.toronto.edu/~kriz/cifar-10-sample/bird6.png"
             },
             {
                 id: 4,
+                labelId: null,
                 src:
                     "https://www.cs.toronto.edu/~kriz/cifar-10-sample/deer6.png"
             },
             {
                 id: 5,
+                labelId: null,
                 src:
                     "https://www.cs.toronto.edu/~kriz/cifar-10-sample/frog10.png"
             },
             {
                 id: 6,
+                labelId: null,
                 src:
                     "https://www.cs.toronto.edu/~kriz/cifar-10-sample/horse2.png"
             },
             {
                 id: 7,
+                labelId: null,
                 src:
                     "https://www.cs.toronto.edu/~kriz/cifar-10-sample/ship1.png"
             },
             {
                 id: 8,
+                labelId: null,
                 src:
                     "https://www.cs.toronto.edu/~kriz/cifar-10-sample/truck4.png"
             },
             {
                 id: 9,
+                labelId: null,
                 src: "https://www.cs.toronto.edu/~kriz/cifar-10-sample/cat9.png"
             }
         ];
 
-        const labels = [
+        const mockLabels = [
             {
                 id: 0,
                 name: "airplane"
@@ -164,46 +188,103 @@ export default Vue.extend({
                 name: "truck"
             }
         ];
-        const selected = 0;
+
+        const count = 0;
+
+        const labelName = "";
+
         return {
             samples,
-            labels: labels,
-            count: 0,
-            maxCount: samples.length - 1,
-            selected
+            mockLabels,
+            labelName,
+            count,
+            maxCount: samples.length - 1
         };
     },
     computed: {
         ...mapState(["loading", "cifarSample", "cifarLabels"]),
-        ...mapGetters(["localLabels", "localImgs"])
+        ...mapGetters(["localLabels", "localImgs"]),
+        filteredDataArray() {
+            const labels: Array<string> = [];
+
+            for (let i = 0; i < this.mockLabels.length; i++) {
+                labels.push(this.mockLabels[i].name);
+            }
+
+            return labels.filter(option => {
+                return (
+                    option
+                        .toString()
+                        .toLowerCase()
+                        .indexOf(this.labelName.toLowerCase()) >= 0
+                );
+            });
+        }
     },
     mounted() {
         // this.$store.dispatch("loadCifarSample");
     },
     methods: {
         click: function() {
-            this.count++;
-            this.send(this.samples[this.count].id.toString(), this.selected);
+            this.send(this.samples[this.count].id, this.labelName);
         },
-        send: function(sampelId: string, labelId: number) {
-            if (labelId != null) {
-                const payload = {
-                    sampleId: sampelId,
-                    label: this.labels[labelId].name
-                };
-                /*
-                // send to api if backend is working
-
-                this.$store.dispatch("cifar/labelCifarSample", {
-                    sampleId: sampelId,
-                    label: this.label[labelId]
-                });
-                 */
-                console.log(payload);
-                console.log(
-                    `Image with Sample ID <${sampelId}> was assigned to Label ID <${labelId}> and successfully send to server.`
-                );
+        send: function(sampelId: number, labelName: string) {
+            if (labelName != "") {
+                const label = this.getLabelByName(labelName);
+                if (label != null) {
+                    this.samples[sampelId].labelId = label.id;
+                    console.log(
+                        `Image with Sample ID <${sampelId}> was assigned to Label "${labelName}" and successfully send to server.`
+                    );
+                    this.next();
+                } else {
+                    console.log(
+                        `Label with name "${labelName}" was not found!`
+                    );
+                }
+                this.focusMethod();
             }
+        },
+        getLabelByName: function(labelName: string) {
+            for (let i = 0; i < this.mockLabels.length; i++) {
+                if (labelName == this.mockLabels[i].name) {
+                    return this.mockLabels[i];
+                }
+            }
+            return null;
+        },
+        getLabelByID: function(labelId: string) {
+            for (let i = 0; i < this.mockLabels.length; i++) {
+                if (labelId == this.mockLabels[i].id) {
+                    return this.mockLabels[i];
+                }
+            }
+            return null;
+        },
+        next: function() {
+            if (this.count < this.mockLabels.length - 1) {
+                this.count++;
+                this.labelName = this.getLabelName(this.count);
+                this.focusMethod();
+            }
+        },
+        previous: function() {
+            if (this.count > 0) {
+                this.count--;
+                this.labelName = this.getLabelName(this.count);
+                this.focusMethod();
+            }
+        },
+        focusMethod: function getFocus() {
+            document.getElementById("input-field").focus();
+            (document.getElementById(
+                "input-field"
+            ) as HTMLInputElement).select();
+        },
+        getLabelName: function(count: number) {
+            if (this.samples[count].labelId != null) {
+                return this.getLabelByID(this.samples[count].labelId).name;
+            } else return "";
         }
     }
 });
@@ -230,6 +311,7 @@ export default Vue.extend({
 #preview {
     opacity: 0.4;
     height: 250px;
+    width: 200px;
 }
 
 .container {
