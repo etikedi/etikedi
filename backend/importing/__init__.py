cf134320cf5b570a5ceb0a38b453c704f68ee23c
Nick Lehmann <nick@lehmann.sh>
2020-07-01 13:42:54 +0200

    Create data directory if not existing

--- backend/importing/__init__.py
+++ backend/importing/__init__.py
@@ -8,8 +8,9 @@ DATA_PATH = (Path(__file__).absolute() / '../../data').resolve()
 
 
 def import_test_datasets():
+    DATA_PATH.mkdir(parents=True, exist_ok=True)
     convert_cifar(DATA_PATH)
-    convert_dwtc(DATA_PATH)
+    # convert_dwtc(DATA_PATH)
     # import_religions_texts(DATA_PATH)
 
 
