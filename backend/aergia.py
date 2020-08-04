1b0884403bffca49d79727f25f322fe14ba980e7
Julius Gonsior <julius@gaenseri.ch>
2020-08-04 15:02:20 +0200

    updated active_learning submodule

--- backend/aergia.py
+++ backend/aergia.py
@@ -19,4 +19,4 @@ with app.app_context():
     api.add_resource(ResumeesListApi, "/api/resumees")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host="0.0.0.0", debug=True)
