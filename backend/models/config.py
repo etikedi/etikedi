42d4b0836951bfbdf2d0cba269a5d06b344492c7
Nick Lehmann <nick@lehmann.sh>
2020-11-16 22:24:19 +0100

    Rework the active learning configuration with pydantic

--- backend/models/config.py
+++ backend/models/config.py
@@ -1,94 +1,53 @@
-from dataclasses import dataclass, field
+from enum import Enum
 
-from marshmallow import validate
-from marshmallow_dataclass import class_schema
-
-
-@dataclass
-class ALConfig:
-    SAMPLING: str = field(
-        metadata={
-            "validate": validate.OneOf(
-                [
-                    "random",
-                    "uncertainty_lc",
-                    "uncertainty_max_margin",
-                    "uncertainty_entropy",
-                ]
-            )
-        }
-    )
-    CLUSTER: str = field(
-        metadata={
-            "validate": validate.OneOf(
-                [
-                    "dummy",
-                    "random",
-                    "MostUncertain_lc",
-                    "MostUncertain_max_margin",
-                    "MostUncertain_entropy",
-                ]
-            )
-        }
-    )
-    MINIMUM_TEST_ACCURACY_BEFORE_RECOMMENDATIONS: float = field(
-        metadata={"validate": validate.Range(min=0, max=1)}
-    )
-    UNCERTAINTY_RECOMMENDATION_CERTAINTY_THRESHOLD: float = field(
-        metadata={"validate": validate.Range(min=0.5, max=1)}
-    )
-    UNCERTAINTY_RECOMMENDATION_RATIO: float = field(
-        metadata={"validate": validate.Range(min=0, max=1)}
-    )
-    CLUSTER_RECOMMENDATION_RATIO_LABELED_UNLABELED: float = field(
-        metadata={"validate": validate.Range(min=0, max=1)}
-    )
-    CLUSTER_RECOMMENDATION_MINIMUM_CLUSTER_UNITY_SIZE: float = field(
-        metadata={"validate": validate.Range(min=0, max=1)}
-    )
-    STOPPING_CRITERIA_UNCERTAINTY: float = field(
-        metadata={"validate": validate.Range(min=0, max=1)}
-    )
-    STOPPING_CRITERIA_ACC: float = field(
-        metadata={"validate": validate.Range(min=0, max=1)}
-    )
-    STOPPING_CRITERIA_STD: float = field(
-        metadata={"validate": validate.Range(min=0, max=1)}
-    )
-    USER_QUERY_BUDGET_LIMIT: float = field(metadata={"validate": validate.Range(min=0)})
-    RANDOM_SEED: float = field(metadata={"validate": validate.Range(min=-1)})
-    NR_QUERIES_PER_ITERATION: int = field(metadata={"validate": validate.Range(min=0)})
-    N_JOBS: int = field(metadata={"validate": validate.Range(min=-1)})
-    NR_LEARNING_ITERATIONS: int = field(metadata={"validate": validate.Range(min=0)})
-    ALLOW_RECOMMENDATIONS_AFTER_STOP: bool
-    WITH_UNCERTAINTY_RECOMMENDATION: bool
-    WITH_CLUSTER_RECOMMENDATION: bool
-    WITH_SNUBA_LITE: bool
-
-    RANDOM_SAMPLE_EVERY: int = field(metadata={"validate": validate.Range(min=5)})
+from pydantic import \
+    confloat as constrained_float, \
+    BaseModel as Schema, \
+    PositiveFloat, PositiveInt
 
 
-ALConfigSchema = class_schema(ALConfig)
+class SamplingMethod(Enum):
+    Random = "random"
+    Uncertainty_lc = "uncertainty_lc"
+    Uncertainty_max_margin = "uncertainty_max_margin"
+    Uncertainty_entropy = "uncertainty_entropy"
 
-default_al_config = ALConfig(
-    SAMPLING="uncertainty_max_margin",
-    CLUSTER="MostUncertain_max_margin",
-    NR_QUERIES_PER_ITERATION=100,
-    WITH_UNCERTAINTY_RECOMMENDATION=True,
-    WITH_CLUSTER_RECOMMENDATION=True,
-    WITH_SNUBA_LITE=False,
-    MINIMUM_TEST_ACCURACY_BEFORE_RECOMMENDATIONS=0,
-    UNCERTAINTY_RECOMMENDATION_CERTAINTY_THRESHOLD=0.99,
-    UNCERTAINTY_RECOMMENDATION_RATIO=0.01,
-    CLUSTER_RECOMMENDATION_RATIO_LABELED_UNLABELED=0.8,
-    CLUSTER_RECOMMENDATION_MINIMUM_CLUSTER_UNITY_SIZE=0.3,
-    ALLOW_RECOMMENDATIONS_AFTER_STOP=True,
-    STOPPING_CRITERIA_UNCERTAINTY=0,
-    STOPPING_CRITERIA_ACC=0,
-    STOPPING_CRITERIA_STD=0,
-    USER_QUERY_BUDGET_LIMIT=2000,
-    RANDOM_SEED=-1,
-    N_JOBS=-1,
-    NR_LEARNING_ITERATIONS=200000,
-    RANDOM_SAMPLE_EVERY=10,
-)
+
+class ClusterMethod(Enum):
+    Dummy = "dummy"
+    Random = "random"
+    MostUncertain_lc = "MostUncertain_lc"
+    MostUncertain_max_margin = "MostUncertain_max_margin"
+    MostUncertain_entropy = "MostUncertain_entropy"
+    
+    
+ZeroToOne = constrained_float(ge=0, le=1)
+OneHalfToOne = constrained_float(ge=0.5, le=1)
+LargerNegativeOne = constrained_float(ge=-1)
+
+
+class ActiveLearningConfig(Schema):
+    SAMPLING: SamplingMethod = SamplingMethod.Uncertainty_max_margin
+    CLUSTER: ClusterMethod = ClusterMethod.MostUncertain_max_margin
+    MINIMUM_TEST_ACCURACY_BEFORE_RECOMMENDATIONS: ZeroToOne = 0
+    UNCERTAINTY_RECOMMENDATION_CERTAINTY_THRESHOLD: OneHalfToOne = 0.99
+    UNCERTAINTY_RECOMMENDATION_RATIO: ZeroToOne = 0.01
+    CLUSTER_RECOMMENDATION_RATIO_LABELED_UNLABELED: ZeroToOne = 0.8
+    CLUSTER_RECOMMENDATION_MINIMUM_CLUSTER_UNITY_SIZE: ZeroToOne = 0.3
+    STOPPING_CRITERIA_UNCERTAINTY: ZeroToOne = 0
+    STOPPING_CRITERIA_ACC: ZeroToOne = 0
+    STOPPING_CRITERIA_STD: ZeroToOne = 0
+    USER_QUERY_BUDGET_LIMIT: PositiveFloat = 2000
+    RANDOM_SEED: LargerNegativeOne = -1
+    N_JOBS: LargerNegativeOne = -1
+    NR_QUERIES_PER_ITERATION: PositiveInt = 100
+    NR_LEARNING_ITERATIONS: PositiveInt = 200000
+    ALLOW_RECOMMENDATIONS_AFTER_STOP: bool = True
+    WITH_UNCERTAINTY_RECOMMENDATION: bool = True
+    WITH_CLUSTER_RECOMMENDATION: bool = True
+    WITH_SNUBA_LITE: bool = False
+
+    RANDOM_SAMPLE_EVERY: PositiveInt = 1
+
+
+default_al_config = ActiveLearningConfig()
--- backend/routes/config.py
+++ backend/routes/config.py
@@ -1,19 +1,16 @@
-import dataclasses
 import json
 
 from fastapi import APIRouter, Depends, HTTPException, status
-from marshmallow import ValidationError
-from pydantic import dataclasses
 from sqlalchemy.orm import Session
 
 from ..active_learning_process import manager
 from ..config import get_db
-from ..models import Dataset, ALConfigSchema
+from ..models import Dataset, ActiveLearningConfig
 
 config_router = APIRouter()
 
 
-@config_router.get("/")
+@config_router.get("/", response_model=ActiveLearningConfig)
 async def get_dataset_config(dataset_id: int, db: Session = Depends(get_db)):
     """ Return the current configuration for the given dataset. """
     dataset = db.query(Dataset).get(dataset_id)
@@ -24,11 +21,11 @@ async def get_dataset_config(dataset_id: int, db: Session = Depends(get_db)):
             detail="No config found."
         )
 
-    return json.loads(dataset.config)
+    return ActiveLearningConfig(**json.loads(dataset.config))
 
 
 @config_router.post("/")
-async def change_dataset_config(dataset_id: int, items, db: Session = Depends(get_db)):
+async def change_dataset_config(dataset_id: int, config: ActiveLearningConfig, db: Session = Depends(get_db)):
     """ Update the configuration for the given dataset. Implies a restart of the AL process. Currently not working. """
     dataset = db.query(Dataset).get(dataset_id)
 
@@ -38,17 +35,7 @@ async def change_dataset_config(dataset_id: int, items, db: Session = Depends(ge
             detail="No dataset found."
         )
 
-    try:
-        new_config = ALConfigSchema().load(
-            {k.upper(): v for k, v in items}
-        )
-    except ValidationError as e:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=e.messages
-        )
-
-    dataset.config = json.dumps(dataclasses.asdict(new_config))
+    dataset.config = json.dumps(config.dict())
     db.commit()
 
     manager.restart_with_config(dataset, json.loads(dataset.config))
