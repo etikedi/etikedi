d0de51ce8b3961dcfc31b0c91d0a85d22ff25441
Nick Lehmann <nick@lehmann.sh>
2020-12-09 18:27:35 +0100

    Explicitly allow frontend url in CORS

--- backend/config/app.py
+++ backend/config/app.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost", "http://localhost:8080", "http://localhost:3000"],
+    allow_origins=["http://localhost", "http://localhost:8080", "http://localhost:3000", "http://localhost:5000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
