a6744686a716c73a76a6e26e8fa4898033a9dc31
Jessica <jessica96@gmx.de>
2020-12-11 17:30:34 +0100

    add filter option

--- backend/routes/datasets.py
+++ backend/routes/datasets.py
@@ -1,11 +1,12 @@
-from typing import List
+from typing import List, Optional, Union
 
-from fastapi import Depends, UploadFile, File, Form, HTTPException, APIRouter, status
+from fastapi import Depends, UploadFile, File, Form, HTTPException, APIRouter, status, Query
+from sqlalchemy import text, func
 
 from ..worker import get_next_sample
 from ..config import db
 from ..importing import import_dataset
-from ..models import Dataset, DatasetDTO, User, Table, Image, Text, SampleDTO
+from ..models import Dataset, DatasetDTO, User, Table, Image, Text, SampleDTO, Sample, Association, Label
 from ..utils import get_current_active_user
 
 dataset_router = APIRouter()
@@ -19,11 +20,11 @@ def get_datasets():
 
 @dataset_router.post("", response_model=DatasetDTO)
 def create_dataset(
-    name: str = Form(...),
-    sample_type: str = Form(...),
-    features: UploadFile = File(...),
-    contents: UploadFile = File(...),
-    current_user: User = Depends(get_current_active_user),
+        name: str = Form(...),
+        sample_type: str = Form(...),
+        features: UploadFile = File(...),
+        contents: UploadFile = File(...),
+        current_user: User = Depends(get_current_active_user),
 ):
     sample_class = {"table": Table, "image": Image, "text": Text}[sample_type]
     if not sample_class:
@@ -59,3 +60,93 @@ def get_first_sample(dataset_id: int):
     first_sample.ensure_string_content()
 
     return first_sample
+
+
+@dataset_router.get("/{dataset_id}/samples", response_model=List[SampleDTO])
+def get_filtered_samples(
+        dataset_id: int,
+        number_elements: Optional[int] = None,
+        labels: Optional[List[int]] = Query(None),
+        users: Optional[List[int]] = Query(None),
+        labeled: Optional[bool] = None,
+        free_text: Optional[Union[str, bytes]] = None,
+        divided_labels: Optional[bool] = None):
+    """
+
+    :param dataset_id:          dataset_id for dataset
+    :param number_elements:     sets max limit how many samples should be returned\\
+    :param labels:              list of label_ids to filter for add each label with label = label_id\\
+    :param users:               list of user_ids to filter for add each user with users = user_id\\
+    :param labeled:             return only labeled samples (true) / unlabeled samples (false)\\
+    :param free_text:           freetext search (only one word)\\
+    :param divided_labels:      search only for samples, which different users labeled differently\\
+    :return:                    list of samples
+    """
+    dataset = db.query(Dataset).filter(Dataset.id == dataset_id)
+
+    if not dataset:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Dataset not found for id: {}.".format(dataset_id)
+        )
+
+    query = db.query(Sample).filter(Sample.dataset_id == dataset_id)
+
+    # JOIN table association for later use
+    if labels or users or divided_labels:
+        query = query.join(Association, Sample.id == Association.sample_id)
+
+    # filter for labels
+    if labels:
+        for label_id in labels:
+            label = db.query(Label).get(label_id)
+            if not label:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail="Label not found for id: {}.".format(label_id),
+                )
+        query = query.filter(Association.label_id.in_(labels))
+
+    # filter for users who labeled the sample
+    if users:
+        for user_id in users:
+            user = db.query(User).get(user_id)
+            if not user:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail="User not found for id: {}.".format(user_id),
+                )
+        query = query.filter(Association.user_id.in_(users))
+
+    # filter for only labeled or unlabeled datasets
+    if labeled is not None:
+        if labeled:
+            query = query.join(Association, Sample.id == Association.sample_id)
+        else:
+            if users or labels:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="Cannot process unlabeled Samples if filters for Labels or Users are set.",
+                )
+            query = query.filter(Sample.dataset_id == 1, ~Sample.associations.any())
+
+    # text search
+    if free_text:
+        sample = db.query(Sample).filter(Sample.dataset_id == dataset_id).first()
+        content_type = sample.type
+
+        if content_type == "text":
+            query = query.join(Text).filter(Text.content.like('%{}%'.format(free_text)))
+
+    # filter for divided labels (sample has more than 1 label)
+    if divided_labels:
+        query = query.group_by(Sample.id).having(func.count(Association.label_id) > 1).order_by(
+            func.count(Association.label_id))
+
+    # limit number of returned elements
+    if number_elements:
+        query = query.limit(number_elements)
+
+    samples = query.all()
+
+    return samples
