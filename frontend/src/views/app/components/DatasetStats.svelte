0935a51b361e8ec8a501d301a33bf8d2e27d61d9
cupcakearmy <hi@nicco.io>
2021-01-17 11:58:40 +0100

    stats

---
+++ frontend/src/views/app/components/DatasetStats.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { humanize } from '../../../lib/human'
+
+  export let statistics
+</script>
+
+<div class="flex items-center">
+  <table>
+    <tr>
+      <td>Total</td>
+      <td class="value">{humanize(statistics.total_samples || 0)}</td>
+      <td>Features</td>
+      <td class="value">{statistics.features || 0}</td>
+    </tr>
+    <tr>
+      <td>Labeled</td>
+      <td class="value">{humanize(statistics.labelled_samples || 0)}</td>
+      <td>Labels</td>
+      <td class="value">{statistics.labels}</td>
+    </tr>
+  </table>
+  <div class="percentage">
+    {Math.round((statistics.labelled_samples / statistics.total_samples) * 100)}%
+  </div>
+</div>
+
+<style>
+  td.value {
+    padding-left: 1em;
+    padding-right: 2em;
+    font-family: monospace;
+    font-weight: bold;
+  }
+
+  .percentage {
+    font-family: monospace;
+    font-weight: bold;
+    font-size: 2em;
+  }
+</style>
