a70440bad3214d9ac3342a02951fb1a95550ca5c
Jessica <jessica96@gmx.de>
2021-01-18 10:30:18 +0100

    Change DB to postgreSQL

--- backend/config/database.py
+++ backend/config/database.py
@@ -6,16 +6,22 @@ from sqlalchemy.orm import sessionmaker
 
 # inside the backend folder
 database_path = Path(__file__).parent.parent.absolute() / 'test.db'
+server_name = "postgres"
+server_password = "admin"
+server_address = "localhost"
+server_port = "5432"
+db_name = "postgres"
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + str(database_path)
+SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://{}:{}@{}:{}/{}" \
+    .format(server_name, server_password, server_address, server_port, db_name)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URI,
-    connect_args={
-        "check_same_thread": SQLALCHEMY_TRACK_MODIFICATIONS,
-        "timeout": 30
-    },
+    SQLALCHEMY_DATABASE_URI
+    # connect_args={
+    #    "check_same_thread": SQLALCHEMY_TRACK_MODIFICATIONS,
+    #    "timeout": 30
+    # },
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
