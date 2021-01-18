03048e5894d3252d106b3be382881928aab5a356
Jessica <jessica96@gmx.de>
2021-01-18 15:33:11 +0100

    Remove unneeded functions and parameters

--- backend/config/database.py
+++ backend/config/database.py
@@ -4,24 +4,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# inside the backend folder
-database_path = Path(__file__).parent.parent.absolute() / 'test.db'
-server_name = "postgres"
+# set access parameters for server and database
+server_username = "postgres"
 server_password = "admin"
-server_address = "localhost"
+server_ipaddress = "localhost"
 server_port = "5432"
 db_name = "postgres"
 
 SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://{}:{}@{}:{}/{}" \
-    .format(server_name, server_password, server_address, server_port, db_name)
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+    .format(server_username, server_password, server_ipaddress, server_port, db_name)
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URI
-    # connect_args={
-    #    "check_same_thread": SQLALCHEMY_TRACK_MODIFICATIONS,
-    #    "timeout": 30
-    # },
+    SQLALCHEMY_DATABASE_URI,
+    connect_args={
+        "connect_timeout": 30
+    },
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
