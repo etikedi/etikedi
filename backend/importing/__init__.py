7cc0d578e7467a46df1d6ce47f1f1167b3d185a4
jkrude <jakob.krude@hotmail.com>
2022-03-26 10:51:34 +0100

    Refined test-dataset-imports

--- backend/importing/__init__.py
+++ backend/importing/__init__.py
@@ -12,21 +12,23 @@ DATA_PATH = (Path(__file__).absolute() / "../../data").resolve()
 def import_test_datasets(data_path: Path = None):
     if data_path is None:
         data_path = DATA_PATH
-    if db.query(Dataset).count():
-        return
     data_path.mkdir(parents=True, exist_ok=True)
+    existing_dataset_names = [d.name for d in db.query(Dataset).all()]
     try:
-        convert_cifar(data_path)
+        if 'CIFAR' not in existing_dataset_names:
+            convert_cifar(data_path)
     except Exception as e:
         error = e.message if hasattr(e, 'message') else str(e)
         logger.warning("Could not import CIFAR dataset. Failed with message: " + error)
     try:
-        convert_dwtc(data_path)
+        if 'DWTC' not in existing_dataset_names:
+            convert_dwtc(data_path)
     except Exception as e:
         error = e.message if hasattr(e, 'message') else str(e)
         logger.warning("Could not import dwtc dataset. Failed with message: " + error)
     try:
-        convert_religions_texts(data_path)
+        if 'Religions Texts' not in existing_dataset_names:
+            convert_religions_texts(data_path)
     except Exception as e:
         error = e.message if hasattr(e, 'message') else str(e)
         logger.warning("Could not import religions_texts dataset. Failed with message: " + error)
