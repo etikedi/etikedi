8fdbaaa9db9bbe7a31673ecf054c91b77c34ae35
Julius Gonsior <julius@gaenseri.ch>
2022-04-08 18:06:32 +0200

    renamed learning curve labels to include the name of the strategies

--- backend/battle_mode/battle_analyzer.py
+++ backend/battle_mode/battle_analyzer.py
@@ -17,27 +17,36 @@ from ..models import (
     Sample,
     ClassificationBoundariesDTO,
     MetricScoresIteration,
-    MetricIteration)
+    MetricIteration,
+)
 from ..utils import zip_unequal
 
 
 class BattleAnalyzer:
-
     def __init__(
-            self,
-            experiment_id: int,
-            dataset_id: int,
-            config: ALBattleConfig,
-            cb_sample: pd.DataFrame,
-            result_one: ExperimentResults,
-            result_two: ExperimentResults):
-        if config is None or cb_sample is None or result_one is None or result_two is None:
+        self,
+        experiment_id: int,
+        dataset_id: int,
+        config: ALBattleConfig,
+        cb_sample: pd.DataFrame,
+        result_one: ExperimentResults,
+        result_two: ExperimentResults,
+    ):
+        if (
+            config is None
+            or cb_sample is None
+            or result_one is None
+            or result_two is None
+        ):
             raise ValueError("At least one parameter was null.")
         self.experiment_id: int = experiment_id
         self.dataset_id: int = dataset_id
         self.config: ALBattleConfig = config
         self.cb_sample: pd.DataFrame = cb_sample
-        self.results: Tuple[ExperimentResults, ExperimentResults] = (result_one, result_two)
+        self.results: Tuple[ExperimentResults, ExperimentResults] = (
+            result_one,
+            result_two,
+        )
         self.metric: Optional[Metric] = None
 
     # Methods for plot related data
@@ -45,14 +54,17 @@ class BattleAnalyzer:
         # convert to long form for altair
 
         def gen(idx, key):
-            return [(it, key, item[MetricsDFKeys.Acc]) for it, item in self.results[idx].metric_scores.iterrows()]
+            return [
+                (it, key, item[MetricsDFKeys.Acc])
+                for it, item in self.results[idx].metric_scores.iterrows()
+            ]
 
-        rows = gen(0, "First")
-        rows += gen(1, "Second")
+        rows = gen(0, self.config.exp_configs[0].QUERY_STRATEGY)
+        rows += gen(1, self.config.exp_configs[1].QUERY_STRATEGY)
+
         return pd.DataFrame(data=rows, columns=["Iteration", "Experiment", "Value"])
 
     def get_confidence_his_data(self) -> Tuple[List[List[float]], List[List[float]]]:
-
         def gen(experiment_idx: int):
             data_over_iterations = []
             for _, row in self.results[experiment_idx].raw_predictions.iterrows():
@@ -62,42 +74,58 @@ class BattleAnalyzer:
         return gen(0), gen(1)
 
     def get_data_map_data(self) -> DataMapsDTO:
-
-        def for_each_iteration(iteration: int, raw_predictions: pd.DataFrame, correct_label_as_idx):
+        def for_each_iteration(
+            iteration: int, raw_predictions: pd.DataFrame, correct_label_as_idx
+        ):
             # use the last 10 iterations as input
             first_iteration = max(0, iteration - 10)
             # include current iteration
-            reduced_frame = raw_predictions.iloc[first_iteration:(iteration + 1), :]
+            reduced_frame = raw_predictions.iloc[first_iteration : (iteration + 1), :]
             data = []
             for smpl in reduced_frame.columns:
                 confidence = reduced_frame[smpl].map(lambda x: max(x)).mean()
                 variance = reduced_frame[smpl].map(lambda x: x.index(max(x))).var()
-                correctness = reduced_frame[smpl].map(lambda x: x.index(max(x)) == correct_label_as_idx[smpl]).mean()
-                data.append({'Confidence': confidence,
-                             'Variability': variance,
-                             'Correctness': correctness,
-                             'SampleID': smpl})
+                correctness = (
+                    reduced_frame[smpl]
+                    .map(lambda x: x.index(max(x)) == correct_label_as_idx[smpl])
+                    .mean()
+                )
+                data.append(
+                    {
+                        "Confidence": confidence,
+                        "Variability": variance,
+                        "Correctness": correctness,
+                        "SampleID": smpl,
+                    }
+                )
             return pd.DataFrame(data)
 
         def gen(exp_idx: int) -> List[pd.DataFrame]:
             r = self.results[exp_idx]
             raw_predictions: pd.DataFrame = r.raw_predictions
-            return [for_each_iteration(iteration, raw_predictions, r.correct_label_as_idx)
-                    for iteration in raw_predictions.index]
+            return [
+                for_each_iteration(iteration, raw_predictions, r.correct_label_as_idx)
+                for iteration in raw_predictions.index
+            ]
 
         return DataMapsDTO(exp_one_data=gen(0), exp_two_data=gen(1))
 
     def _use_pca_for_feature_selection(self, exclude: List[int]):
         dataset = db.get(Dataset, self.dataset_id)
         feature_names = dataset.feature_names
-        samples = [s for s in
-                   db.query(Sample).filter(Sample.dataset_id == self.dataset_id).filter(Sample.id.not_in(exclude)).all()
-                   if s.labels != []]
+        samples = [
+            s
+            for s in db.query(Sample)
+            .filter(Sample.dataset_id == self.dataset_id)
+            .filter(Sample.id.not_in(exclude))
+            .all()
+            if s.labels != []
+        ]
 
-        samples_df = pd.DataFrame([
-            [smpl.id] + smpl.extract_feature_list()
-            for smpl in samples],
-            columns=["SampleID"] + feature_names.split(","))
+        samples_df = pd.DataFrame(
+            [[smpl.id] + smpl.extract_feature_list() for smpl in samples],
+            columns=["SampleID"] + feature_names.split(","),
+        )
         pca = PCA(n_components=2)
         transformed_samples = pca.fit_transform(X=samples_df.iloc[:, 1:])
         pca_df = pd.DataFrame(data=transformed_samples, columns=["PCA1", "PCA2"])
@@ -105,32 +133,45 @@ class BattleAnalyzer:
         pca_df.set_index("SampleID")
         return pca_df
 
-    def _use_names_for_feature_selection(self, name_one: str, name_two: str, exclude: List[int]):
-        samples = [s for s in
-                   db.query(Sample).filter(Sample.dataset_id == self.dataset_id).filter(Sample.id.not_in(exclude)).all()
-                   if s.labels != []]
-        all_samples: pd.DataFrame = pd.DataFrame([
-            {
-                "SampleID": smpl.id,
-                name_one: smpl.feature_dict()[name_one],
-                name_two: smpl.feature_dict()[name_two]
-            }
-            for smpl in samples]
+    def _use_names_for_feature_selection(
+        self, name_one: str, name_two: str, exclude: List[int]
+    ):
+        samples = [
+            s
+            for s in db.query(Sample)
+            .filter(Sample.dataset_id == self.dataset_id)
+            .filter(Sample.id.not_in(exclude))
+            .all()
+            if s.labels != []
+        ]
+        all_samples: pd.DataFrame = pd.DataFrame(
+            [
+                {
+                    "SampleID": smpl.id,
+                    name_one: smpl.feature_dict()[name_one],
+                    name_two: smpl.feature_dict()[name_two],
+                }
+                for smpl in samples
+            ]
         )
         all_samples.set_index("SampleID")
         return all_samples
 
     def get_vector_space_data(self) -> Tuple[List[pd.DataFrame], List[pd.DataFrame]]:
-        """ @return for each experiment for each iteration a data-frame with columns:
-                <feature-one>, <feature-two>, SampleID, Color"""
+        """@return for each experiment for each iteration a data-frame with columns:
+        <feature-one>, <feature-two>, SampleID, Color"""
 
         def gen(exp_idx: int):
             r = self.results[exp_idx]
             # reduced_features_df['SampleID'] == all training sample
             test_sample = list(map(lambda id_: int(id_), r.raw_predictions.columns))
-            reduced_features_df = self._use_pca_for_feature_selection(exclude=test_sample) \
-                if self.config.PLOT_CONFIG.FEATURES is None \
-                else self._use_names_for_feature_selection(*self.config.PLOT_CONFIG.FEATURES, exclude=test_sample)
+            reduced_features_df = (
+                self._use_pca_for_feature_selection(exclude=test_sample)
+                if self.config.PLOT_CONFIG.FEATURES is None
+                else self._use_names_for_feature_selection(
+                    *self.config.PLOT_CONFIG.FEATURES, exclude=test_sample
+                )
+            )
 
             labeled_ids = r.initially_labeled.copy()
             iterations = []
@@ -138,14 +179,18 @@ class BattleAnalyzer:
                 it_df: pd.DataFrame = reduced_features_df.copy()
                 selected_ids = meta_data.sample_ids
                 labeled_ids += selected_ids
-                it_df['Color'] = it_df['SampleID'].apply(lambda ID: classify(ID, selected_ids, labeled_ids))
+                it_df["Color"] = it_df["SampleID"].apply(
+                    lambda ID: classify(ID, selected_ids, labeled_ids)
+                )
                 iterations.append(it_df)
             return iterations
 
         def classify(ID, selected_ids, labeled_ids):
-            return 'Selected' if ID in selected_ids \
-                else ('Labeled' if ID in labeled_ids
-                      else 'Unlabeled')
+            return (
+                "Selected"
+                if ID in selected_ids
+                else ("Labeled" if ID in labeled_ids else "Unlabeled")
+            )
 
         return gen(0), gen(1)
 
@@ -155,22 +200,30 @@ class BattleAnalyzer:
             # ndarray with list of rows of [feature_1, feature_2]
             reduced_features = pd.DataFrame(
                 PCA(n_components=2).fit_transform(self.cb_sample),
-                columns=['PCA1', 'PCA2'])
+                columns=["PCA1", "PCA2"],
+            )
         else:
             reduced_features = self.cb_sample.loc[:, self.config.PLOT_CONFIG.FEATURES]
 
         def for_each_iteration(iteration: pd.Series, classes: List[str]):
-            return pd.DataFrame(data={
-                'Class': [classes[confidence_scores.index(max(confidence_scores))] for _, confidence_scores in
-                          iteration.iteritems()],
-                'Confidence': [max(item) for _, item in iteration.iteritems()]
-            })
+            return pd.DataFrame(
+                data={
+                    "Class": [
+                        classes[confidence_scores.index(max(confidence_scores))]
+                        for _, confidence_scores in iteration.iteritems()
+                    ],
+                    "Confidence": [max(item) for _, item in iteration.iteritems()],
+                }
+            )
 
         def gen(exp_idx):
             result = self.results[exp_idx]
             assert len(result.cb_predictions.columns) == len(self.cb_sample)
             # get predicted class and confidence for pseudo samples
-            iteration_list = [for_each_iteration(row, result.classes) for _, row in result.cb_predictions.iterrows()]
+            iteration_list = [
+                for_each_iteration(row, result.classes)
+                for _, row in result.cb_predictions.iterrows()
+            ]
             return iteration_list
 
         return ClassificationBoundariesDTO(
@@ -178,7 +231,8 @@ class BattleAnalyzer:
             exp_one_iterations=gen(0),
             exp_two_iterations=gen(1),
             x_bins=self.config.PLOT_CONFIG.CLASSIFICATION_BOUNDARIES.MAX_X_BINS,
-            y_bins=self.config.PLOT_CONFIG.CLASSIFICATION_BOUNDARIES.MAX_Y_BINS)
+            y_bins=self.config.PLOT_CONFIG.CLASSIFICATION_BOUNDARIES.MAX_Y_BINS,
+        )
 
     def get_metrics(self) -> Metric:
         if self.metric is not None:
@@ -187,14 +241,17 @@ class BattleAnalyzer:
         def extract_per_exp(idx: int):
             r = self.results[idx]
             assert len(r.meta_data) == len(r.metric_scores.index)
-            scores: List = [MetricScoresIteration.of(row[1]) for row in r.metric_scores.iterrows()]
-            return [MetricIteration(meta=r.meta_data[i], metrics=scores[i]) for i in range(len(scores))]
+            scores: List = [
+                MetricScoresIteration.of(row[1]) for row in r.metric_scores.iterrows()
+            ]
+            return [
+                MetricIteration(meta=r.meta_data[i], metrics=scores[i])
+                for i in range(len(scores))
+            ]
 
         self.metric = Metric(
-            iterations=list(zip_unequal(
-                extract_per_exp(0),
-                extract_per_exp(1))),
-            percentage_similar=self._percentage_similar_samples()
+            iterations=list(zip_unequal(extract_per_exp(0), extract_per_exp(1))),
+            percentage_similar=self._percentage_similar_samples(),
         )
         return self.metric
 
@@ -211,8 +268,8 @@ class BattleAnalyzer:
         assert len(samples_one) == len(samples_two)
         similar_per_iteration = []
         for i in range(len(samples_one)):
-            s_one = set(np.concatenate(samples_one[:i + 1]))
-            s_two = set(np.concatenate(samples_two[:i + 1]))
+            s_one = set(np.concatenate(samples_one[: i + 1]))
+            s_two = set(np.concatenate(samples_two[: i + 1]))
             similar_samples = len(s_one.intersection(s_two))
             similar_percentage = similar_samples / len(s_one.union(s_two))
             similar_per_iteration.append(similar_percentage)
